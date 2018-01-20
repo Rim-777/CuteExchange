a237f4942d40d7586b55d522972051c8f258fcdc
Rim-777 <javatimomoss@gmail.com>
2018-01-20 22:39:38 +0200

    added the method convert_to with private deligates and tests for them

--- lib/cute_exchange/money.rb
+++ lib/cute_exchange/money.rb
@@ -1,4 +1,8 @@
+require 'bigdecimal'
+
 class Money
+  include Comparable
+
   class << self
     attr_reader :base, :rates
 
@@ -6,5 +10,37 @@ class Money
       @base = base
       @rates = rates
     end
+  end
+
+  attr_reader :amount, :currency
+
+  def initialize(amount, currency)
+    @amount = amount
+    @currency = currency
+  end
+
+  def convert_to(target)
+    return self if target == currency
+    result = target == Money.base ? convert_to_base : convert_to_base * rate_by(target)
+    self.class.new(result.to_s('F'), target)
+  end
+
+  def inspect(options={})
+    precision = options[:precision] || 2
+    "#{"%.#{precision}f" % amount} #{currency}"
+  end
+
+  def <=>(arg)
+    BigDecimal(amount.to_s)  <=> BigDecimal(arg.convert_to(currency).amount.to_s)
+  end
+
+  private
+  def convert_to_base
+    return BigDecimal(amount.to_s) if currency == Money.base
+    BigDecimal(amount.to_s) /  Money.rates.fetch(currency)
+  end
+
+  def rate_by(currency)
+    Money.rates.fetch(currency)
   end
 end
--- spec/cute_exchange/money_spec.rb
+++ spec/cute_exchange/money_spec.rb
@@ -1,11 +1,111 @@
 require 'spec_helper'
 
 RSpec.describe Money do
+  let(:fifty_eur) {Money.new(50, 'EUR')}
+  let(:fifty_five_dollars_fifty_cent) {Money.new(55.5, 'USD')}
+
+  before do
+    Money.conversion_rates('EUR', 'USD' => 1.11, 'RUB' => 70)
+  end
+
   describe '.conversion_rates' do
     it 'assigns right values' do
-      Money.conversion_rates('EUR', 'USD' => 1.11, 'RUB' => 70)
       expect(Money.base).to eq 'EUR'
       expect(Money.rates).to eq 'USD' => 1.11, 'RUB' => 70
+    end
+  end
+
+  describe '#attr_readers' do
+    describe '#amount' do
+      it 'returns 50' do
+        expect(fifty_eur.amount).to eq 50
+      end
+    end
+
+    describe '#currency' do
+      it 'returns EUR' do
+        expect(fifty_eur.currency).to eq 'EUR'
+      end
+    end
+  end
+
+  describe '#inspect' do
+    it 'precents the money instance with default precision' do
+      expect(fifty_eur.inspect).to eq '50.00 EUR'
+    end
+
+    it 'precents the money instance with the given precision' do
+      expect(Money.new(0.1234567, 'Bitcoin').inspect(precision: 4)).to eq '0.1235 Bitcoin'
+    end
+  end
+
+  describe '#convert_to' do
+    context 'convert the base currency to other currency' do
+      it 'returns the Money instans with currency: USD and amount: 50*1.11' do
+        expect(fifty_eur.convert_to('USD')).to eq Money.new(55.5, 'USD')
+      end
+
+      it 'receives the method convert_to_base for the Money instance' do
+        expect(fifty_eur).to receive(:convert_to_base).and_call_original
+        fifty_eur.convert_to('USD')
+      end
+
+      it 'receives the method rate_by for the Money instance with USD as an argument' do
+        expect(fifty_eur).to receive(:rate_by).with('USD').and_call_original
+        fifty_eur.convert_to('USD')
+      end
+    end
+
+    context 'convert an other currency to the base currency' do
+      it 'returns the Money instance with the currency: EUR and amount 50' do
+        expect(fifty_five_dollars_fifty_cent.convert_to('EUR')).to eq Money.new(50, 'EUR')
+      end
+
+      it 'receives the method convert_to_base for the Money instance' do
+        expect(fifty_five_dollars_fifty_cent).to receive(:convert_to_base).and_call_original
+        fifty_five_dollars_fifty_cent.convert_to('EUR')
+      end
+    end
+
+    context 'convert an other currency to another currency' do
+      it 'returns the Money instance with the currency:RUB and amount 3500' do
+        expect(fifty_five_dollars_fifty_cent.convert_to('RUB')).to eq Money.new(3500, 'RUB')
+      end
+
+      it 'receives the method convert_to_base for the Money instance' do
+        expect(fifty_five_dollars_fifty_cent).to receive(:convert_to_base).and_call_original
+        fifty_five_dollars_fifty_cent.convert_to('RUB')
+      end
+
+      it 'receives the method rate_by for the Money instance with USD as an argument' do
+        expect(fifty_five_dollars_fifty_cent).to receive(:rate_by).with('RUB').and_call_original
+        fifty_five_dollars_fifty_cent.convert_to('RUB')
+      end
+    end
+  end
+
+  describe '#convert_to_base' do
+    it 'returns a BigDecimal instance' do
+      expect(fifty_five_dollars_fifty_cent.send(:convert_to_base)).to be_a(BigDecimal)
+    end
+
+    context 'the method is called for the base currency' do
+      it 'returns the ammount of the base currency' do
+        expect(fifty_eur.send(:convert_to_base)).to eq fifty_eur.amount
+      end
+    end
+
+    context 'the method is called for the other currency' do
+      it 'returns the converted ammount of the currency' do
+        expect(fifty_five_dollars_fifty_cent.send(:convert_to_base)).to eq 50
+      end
+    end
+  end
+
+  describe '#rate_by(currency)' do
+    it 'returns the rate of the given currency from the established rates' do
+      expect(fifty_eur.send(:rate_by, 'USD')).to eq 1.11
+      expect(fifty_eur.send(:rate_by, 'RUB')).to eq 70
     end
   end
 end
